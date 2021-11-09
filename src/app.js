b044f039b6e94176b467193738ff1c906a79761d
AzizbekTokhirjonov <azizwebdev77@gmail.com>
2021-11-09 15:33:43 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import { productsRouter } from "./services/products/index.js";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.get('/test', (req, res) => {
-    res.send({ message: 'Test success' })
-})
+app.get("/test", (req, res) => {
+  res.send({ message: "Test success" });
+});
 
-app.use("/products", productsRouter)
-
-export { app }
+app.use("/products", productsRouter);
+console.log("Update?");
+export { app };
